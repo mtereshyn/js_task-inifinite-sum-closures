5cbcd7e277ee69f5b203b314d03cc3e5adf5ea40
Maksym Tereshyn <max.tereshyn@gmail.com>
2024-12-09 07:59:36 +0100

    feat: solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -4,7 +4,22 @@
  * @return {function}
  */
 function makeInfinityAdder() {
-  // write code here
+  let total = 0;
+
+  const adder = (num) => {
+    if(num === undefined) {
+      const currentTotal = total;
+      currentTotal = 0;
+
+      return currentTotal;
+    }
+
+    total += num;
+
+    return adder;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
